022cabc88545c521e23fa0024244e9d7f65a8aeb
GrigoriyMo <kovarscky.g@yandex.ru>
2024-08-27 02:06:40 +0300

    webhooks page update

--- pages/webhooks.vue
+++ pages/webhooks.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    WEBHOOKS
+    <div class="flex">
+
+      <List  />
+    </div>
   </div>
 </template>
 
@@ -8,6 +11,4 @@
 
 </script>
 
-<style>
-
-</style>
+<style></style>
